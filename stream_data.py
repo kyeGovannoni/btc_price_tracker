1c45f8dea655f6ac18b2de1f4d8a8936637d2115
Kye Giovannoni <68851818+kyeGovannoni@users.noreply.github.com>
2022-10-21 09:16:07 +0000

    Update stream_data.py

--- stream_data.py
+++ stream_data.py
@@ -11,17 +11,9 @@ def main(address ='wss://stream.binance.com:9443/ws/btcgbp@kline_1m',
     
     if asyncio.get_event_loop().is_closed:
         asyncio.set_event_loop(asyncio.new_event_loop())
+    
     loop: AbstractEventLoop = asyncio.get_event_loop()
-
-    #try:
-    #    loop.run_until_complete(
-    #                        BinaceWebsocket(**config).open_connection(parser= parser)
-    #                          )
-    #except KeyboardInterrupt as e:
-    #    print(colorama.Fore.RED,'Closing Stream.')
-    #    
-    #    loop.close()
-
+        
     try:
         loop.set_debug(False)
         loop.run_until_complete(BinaceWebsocket(**config).open_connection(parser= parser))
